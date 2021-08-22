eee090c1402ba4cb061ec5ad22a3fa640402d539
tomashoffer <tomashoffer96@gmail.com>
2021-08-22 20:05:14 +0300

    j

--- server.ts
+++ server.ts
@@ -1,1 +1,22 @@
-ghg
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000 
+const cookieParser = require('cookie-parser');
+const fs = require("fs");
+
+export const localJson = () => {
+    const fileJson = fs.readFileSync("./db/users.json");
+    return JSON.parse(fileJson);
+  };
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+// IMPORT ROUTES FILES
+
+// ROUTES
+
+
+app.listen(port, () => { console.log('listen on 3000') })
