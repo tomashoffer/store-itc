e7eb8968bae44c07e1ec2084bbc21a1a2097c536
tomashoffer <tomashoffer96@gmail.com>
2021-08-29 17:38:31 +0300

    COMIT

--- public/js/dist/store.dev.js
+++ public/js/dist/store.dev.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var getAllProductos = function getAllProductos() {
+window.onload = setTimeout(function getAllProductos() {
   var getproductos, productos, getLogIn, role;
   return regeneratorRuntime.async(function getAllProductos$(_context) {
     while (1) {
@@ -26,38 +26,54 @@ var getAllProductos = function getAllProductos() {
       }
     }
   });
-};
+}, 670);
 
 function renderProducts(productos, role) {
-  var root = document.querySelector(".grid");
-  var html = "";
-  var addLink = document.querySelector('.addNav');
+  var root, html, addLink;
+  return regeneratorRuntime.async(function renderProducts$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          root = document.querySelector(".grid");
+          html = "";
+          addLink = document.querySelector('.addNav');
 
-  if (role === "admin") {
-    addLink.style.display = 'inline';
-    productos.forEach(function (prod) {
-      html += "   <div class=\"producto\">\n        <a href=\"producto.html\" onclick='selectedProd(\"".concat(prod.id, "\")'>\n            <img class=\"producto__imagen\" src=\"").concat(prod.productImage, "\" alt=\"imagen camisa\">\n            <div class=\"producto__informacion\">\n                <p class=\"producto__nombre\">").concat(prod.productName, "</p>\n              \n                <p class=\"producto__precio\">$").concat(prod.productPrice, "</p>\n                <p class=\"producto__precio\">Stock: ").concat(prod.stock, "</p>\n                <div class=\"producto_iconos\">\n                <a href=\"\" data-bs-toggle=\"modal\" data-bs-target=\"#exampleModal\" onclick='editProdId(\"").concat(prod.id, "\")'><i class=\"edit_icon fas fa-edit\"></i></a>\n                <a href=\"\" class=\"hola\" onclick='deleteProdId(\"").concat(prod.id, "\")'><i class=\"delete_icon fas fa-trash\"></i></a>\n                </div>\n            </div>\n        </a>\n    </div> ");
-    });
-  } else {
-    productos.forEach(function (prod) {
-      html += "   <div class=\"producto\">\n        <a href=\"producto.html\" onclick='selectedProd(\"".concat(prod.id, "\")'>\n            <img class=\"producto__imagen\" src=\"").concat(prod.productImage, "\" alt=\"imagen camisa\">\n            <div class=\"producto__informacion\">\n                <p class=\"producto__nombre\">").concat(prod.productName, "</p>\n                <p class=\"producto__precio\">$").concat(prod.productPrice, "</p>\n                <p class=\"producto__precio\">Stock: ").concat(prod.stock, "</p>\n            </div>\n        </a>\n    </div> ");
-    });
-  }
+          if (role === "admin") {
+            addLink.style.display = 'inline';
+            productos.forEach(function (prod) {
+              html += "   <div class=\"producto\">\n        <a href=\"producto.html\" onclick='selectedProd(\"".concat(prod.id, "\")'>\n            <img class=\"producto__imagen\" src=\"").concat(prod.productImage, "\" alt=\"imagen camisa\">\n            <div class=\"producto__informacion\">\n                <p class=\"producto__nombre\">").concat(prod.productName, "</p>\n              \n                <p class=\"producto__precio\">$").concat(prod.productPrice, "</p>\n                <p class=\"producto__precio\">Stock: ").concat(prod.stock, "</p>\n                <div class=\"producto_iconos\">\n                <a  data-bs-toggle=\"modal\" data-bs-target=\"#exampleModal\" onclick='editProdId(\"").concat(prod.id, "\")'><i onclick='editProdId(\"").concat(prod.id, "\")' class=\"edit_icon fas fa-edit\"></i></a>\n               <i onclick='deleteProdId(\"").concat(prod.id, "\")' style=\"cursor:pointer\" class=\"delete_icon fas fa-trash\"></i>\n                </div>\n            </div>\n        </a>\n    </div> ");
+            });
+          } else {
+            productos.forEach(function (prod) {
+              html += "   <div class=\"producto\">\n        <a href=\"producto.html\" onclick='selectedProd(\"".concat(prod.id, "\")'>\n            <img class=\"producto__imagen\" src=\"").concat(prod.productImage, "\" alt=\"imagen camisa\">\n            <div class=\"producto__informacion\">\n                <p class=\"producto__nombre\">").concat(prod.productName, "</p>\n                <p class=\"producto__precio\">$").concat(prod.productPrice, "</p>\n                <p class=\"producto__precio\">Stock: ").concat(prod.stock, "</p>\n            </div>\n        </a>\n    </div> ");
+            });
+          }
 
-  root.innerHTML = html;
+          root.innerHTML = html;
+
+        case 5:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
 }
 
 function selectedProd(id) {
   var postIdSelectedProd;
-  return regeneratorRuntime.async(function selectedProd$(_context2) {
+  return regeneratorRuntime.async(function selectedProd$(_context3) {
     while (1) {
-      switch (_context2.prev = _context2.next) {
+      switch (_context3.prev = _context3.next) {
         case 0:
-          postIdSelectedProd = axios.post("/product/".concat(id));
+          _context3.next = 2;
+          return regeneratorRuntime.awrap(axios.post("/product/".concat(id)));
 
-        case 1:
+        case 2:
+          postIdSelectedProd = _context3.sent;
+
+        case 3:
         case "end":
-          return _context2.stop();
+          return _context3.stop();
       }
     }
   });
@@ -83,20 +99,20 @@ function handleAddProd(e) {
 
 function postProd(newProd) {
   var response;
-  return regeneratorRuntime.async(function postProd$(_context3) {
+  return regeneratorRuntime.async(function postProd$(_context4) {
     while (1) {
-      switch (_context3.prev = _context3.next) {
+      switch (_context4.prev = _context4.next) {
         case 0:
-          _context3.next = 2;
+          _context4.next = 2;
           return regeneratorRuntime.awrap(axios.post('/product/addProducts', newProd));
 
         case 2:
-          response = _context3.sent;
+          response = _context4.sent;
           console.log(response);
 
         case 4:
         case "end":
-          return _context3.stop();
+          return _context4.stop();
       }
     }
   });
@@ -122,29 +138,18 @@ function handleEditModal(e) {
 
 function editProductData(newProdData) {
   var editId;
-  return regeneratorRuntime.async(function editProductData$(_context4) {
-    while (1) {
-      switch (_context4.prev = _context4.next) {
-        case 0:
-          editId = axios.post("/product/edit", newProdData);
-
-        case 1:
-        case "end":
-          return _context4.stop();
-      }
-    }
-  });
-}
-
-function editProdId(id) {
-  var editId;
-  return regeneratorRuntime.async(function editProdId$(_context5) {
+  return regeneratorRuntime.async(function editProductData$(_context5) {
     while (1) {
       switch (_context5.prev = _context5.next) {
         case 0:
-          editId = axios.post("/product/edit/".concat(id));
+          _context5.next = 2;
+          return regeneratorRuntime.awrap(axios.post("/product/edit", newProdData));
 
-        case 1:
+        case 2:
+          editId = _context5.sent;
+          refresh();
+
+        case 4:
         case "end":
           return _context5.stop();
       }
@@ -152,20 +157,19 @@ function editProdId(id) {
   });
 }
 
-function deleteProdId(id) {
-  var deleteId;
-  return regeneratorRuntime.async(function deleteProdId$(_context6) {
+function editProdId(id) {
+  var editId;
+  return regeneratorRuntime.async(function editProdId$(_context6) {
     while (1) {
       switch (_context6.prev = _context6.next) {
         case 0:
           _context6.next = 2;
-          return regeneratorRuntime.awrap(axios.post("/product/delete/".concat(id)));
+          return regeneratorRuntime.awrap(axios.post("/product/edit/".concat(id)));
 
         case 2:
-          deleteId = _context6.sent;
-          getAllProductos();
+          editId = _context6.sent;
 
-        case 4:
+        case 3:
         case "end":
           return _context6.stop();
       }
@@ -173,36 +177,91 @@ function deleteProdId(id) {
   });
 }
 
+function deleteProdId(id) {
+  return regeneratorRuntime.async(function deleteProdId$(_context7) {
+    while (1) {
+      switch (_context7.prev = _context7.next) {
+        case 0:
+          _context7.next = 2;
+          return regeneratorRuntime.awrap(axios.post("/product/delete/".concat(id)));
+
+        case 2:
+          refresh();
+
+        case 3:
+        case "end":
+          return _context7.stop();
+      }
+    }
+  });
+}
+
 var btnSubmit = document.querySelector('.btnSubmit');
-btnSubmit.addEventListener('click', function () {
-  getAllProductos();
-  window.location.reload();
+btnSubmit.addEventListener('click', function _callee() {
+  return regeneratorRuntime.async(function _callee$(_context8) {
+    while (1) {
+      switch (_context8.prev = _context8.next) {
+        case 0:
+          window.location.reload();
+
+        case 1:
+        case "end":
+          return _context8.stop();
+      }
+    }
+  });
 });
 var btnSubmitAdd = document.querySelector('.btnSubmitAdd');
-btnSubmitAdd.addEventListener('click', function () {
-  getAllProductos();
-  window.location.reload();
-}); // SEARCH BAR
+btnSubmitAdd.addEventListener('click', function _callee2() {
+  return regeneratorRuntime.async(function _callee2$(_context9) {
+    while (1) {
+      switch (_context9.prev = _context9.next) {
+        case 0:
+          refresh();
+
+        case 1:
+        case "end":
+          return _context9.stop();
+      }
+    }
+  });
+});
+
+function refresh() {
+  return regeneratorRuntime.async(function refresh$(_context10) {
+    while (1) {
+      switch (_context10.prev = _context10.next) {
+        case 0:
+          window.location.reload();
+
+        case 1:
+        case "end":
+          return _context10.stop();
+      }
+    }
+  });
+} // SEARCH BAR
+
 
 function regExSurvey(searchBar) {
   var getproductos, productos, getLogIn, role, newArray, _loop, i;
 
-  return regeneratorRuntime.async(function regExSurvey$(_context7) {
+  return regeneratorRuntime.async(function regExSurvey$(_context11) {
     while (1) {
-      switch (_context7.prev = _context7.next) {
+      switch (_context11.prev = _context11.next) {
         case 0:
-          _context7.prev = 0;
-          _context7.next = 3;
+          _context11.prev = 0;
+          _context11.next = 3;
           return regeneratorRuntime.awrap(axios('/product/getProducts'));
 
         case 3:
-          getproductos = _context7.sent;
+          getproductos = _context11.sent;
           productos = getproductos.data;
-          _context7.next = 7;
+          _context11.next = 7;
           return regeneratorRuntime.awrap(axios('/user/logIn'));
 
         case 7:
-          getLogIn = _context7.sent;
+          getLogIn = _context11.sent;
           role = getLogIn.data.role;
           newArray = [];
 
@@ -219,17 +278,17 @@ function regExSurvey(searchBar) {
           }
 
           renderProducts(newArray, role);
-          _context7.next = 18;
+          _context11.next = 18;
           break;
 
         case 15:
-          _context7.prev = 15;
-          _context7.t0 = _context7["catch"](0);
-          console.error(_context7.t0);
+          _context11.prev = 15;
+          _context11.t0 = _context11["catch"](0);
+          console.error(_context11.t0);
 
         case 18:
         case "end":
-          return _context7.stop();
+          return _context11.stop();
       }
     }
   }, null, null, [[0, 15]]);
@@ -251,20 +310,20 @@ searchBar.addEventListener('keyup', searchProduct); // LOGOUT
 
 function logOut() {
   var logOut;
-  return regeneratorRuntime.async(function logOut$(_context8) {
+  return regeneratorRuntime.async(function logOut$(_context12) {
     while (1) {
-      switch (_context8.prev = _context8.next) {
+      switch (_context12.prev = _context12.next) {
         case 0:
-          _context8.next = 2;
+          _context12.next = 2;
           return regeneratorRuntime.awrap(axios("/user/logOut"));
 
         case 2:
-          logOut = _context8.sent;
+          logOut = _context12.sent;
           window.location.href = "http://localhost:3000/";
 
         case 4:
         case "end":
-          return _context8.stop();
+          return _context12.stop();
       }
     }
   });
--- public/js/store.js
+++ public/js/store.js
@@ -1,13 +1,13 @@
-const getAllProductos = async () => {
-    const getproductos = await axios('/product/getProducts');
-    const productos = getproductos.data;
-    const getLogIn = await axios('/user/logIn');
-    const role = getLogIn.data.role;
-    renderProducts(productos, role);
-}
+window.onload = setTimeout(async function getAllProductos(){
 
+  const getproductos = await axios('/product/getProducts');
+  const productos = getproductos.data;
+  const getLogIn = await axios('/user/logIn');
+  const role = getLogIn.data.role;
+ renderProducts(productos, role);
+}, 670)
 
-function renderProducts(productos, role){
+async function renderProducts(productos, role){
     const root = document.querySelector(".grid");
     let html = "";
     const addLink = document.querySelector('.addNav');
@@ -23,8 +23,8 @@ function renderProducts(productos, role){
                 <p class="producto__precio">$${prod.productPrice}</p>
                 <p class="producto__precio">Stock: ${prod.stock}</p>
                 <div class="producto_iconos">
-                <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick='editProdId("${prod.id}")'><i class="edit_icon fas fa-edit"></i></a>
-                <a href="" class="hola" onclick='deleteProdId("${prod.id}")'><i class="delete_icon fas fa-trash"></i></a>
+                <a  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick='editProdId("${prod.id}")'><i onclick='editProdId("${prod.id}")' class="edit_icon fas fa-edit"></i></a>
+               <i onclick='deleteProdId("${prod.id}")' style="cursor:pointer" class="delete_icon fas fa-trash"></i>
                 </div>
             </div>
         </a>
@@ -48,7 +48,7 @@ function renderProducts(productos, role){
   }  
 
   async function selectedProd(id){
-    const postIdSelectedProd = axios.post(`/product/${id}`);
+    const postIdSelectedProd = await axios.post(`/product/${id}`);
   }
   
 // MODAL ADD
@@ -81,26 +81,30 @@ function handleEditModal(e){
 }
 
 async function editProductData(newProdData){
-  const editId = axios.post(`/product/edit`, newProdData);
+  const editId = await axios.post(`/product/edit`, newProdData);
+  refresh()
 }
+
 
 async function editProdId(id){
-    const editId = axios.post(`/product/edit/${id}`);
+    const editId = await axios.post(`/product/edit/${id}`);
 }
 async function deleteProdId(id){
-    const deleteId = await axios.post(`/product/delete/${id}`);
-    getAllProductos();
-}
+    await axios.post(`/product/delete/${id}`)
+    refresh()
+    }
 
 const btnSubmit = document.querySelector('.btnSubmit');
-btnSubmit.addEventListener('click', () => {
-  getAllProductos();
+btnSubmit.addEventListener('click', async () => {
   window.location.reload();})
 
 const btnSubmitAdd = document.querySelector('.btnSubmitAdd');
-btnSubmitAdd.addEventListener('click', () => {
-  getAllProductos();
-  window.location.reload();});
+btnSubmitAdd.addEventListener('click', async() => {
+  refresh();});
+
+async function refresh() {
+  window.location.reload();
+}
 
 // SEARCH BAR
   async function regExSurvey(searchBar){
